7e825c673fe0bf59888af4d0ac7e7f66b3a6473a
Alan Boaventura <alanboaventurab@gmail.com>
2019-04-28 22:15:32 -0300

    Ajustar estrutura do projeto

--- caixeiroviajante/Main.py
+++ caixeiroviajante/Main.py
@@ -1,31 +1,29 @@
 import numpy as np
 import random
-from pack.funcaoAptidao import apt_func
-# matriz da populacao com 20 membros
-populacao = np.zeros((20, 20), dtype=np.float)
+from caixeiroviajante.FuncaoAptidao import apt_func
 
-# variaveis
+# Variaveis
 nCidades = 20
 nGeracoes = 10000
 
-# define populacao aleatoria
+# Matriz da populacao
+populacao = np.zeros((nCidades, nCidades), dtype=np.float)
+
+# Define populacao aleatoria
 for i in range(nCidades):
     populacao[i] = random.sample(range(nCidades), nCidades)
-
-# for i in range(nCidades):
-#    print populacao[i]
 
 x = np.random.rand(1, nCidades)
 y = np.random.rand(1, nCidades)
 
 lista_aptidao = apt_func(populacao[:, :], x, y, nCidades)
-print lista_aptidao
+print(lista_aptidao)
 
 populacao_ordenada = np.zeros((20, 20), dtype=np.float)
 
 for i in range(nCidades):
-    populacao_ordenada[i, :] = populacao[int(lista_aptidao[i,0]), :]
+    populacao_ordenada[i, :] = populacao[int(lista_aptidao[i, 0]), :]
 
-print populacao_ordenada
+print(populacao_ordenada)
 
-# funcao de apitadao
+
