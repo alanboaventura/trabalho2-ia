8f1765b7914de4d404df295ae4d443fe0d1d11b6
Lucas Carvalho <lucascarvalho.bud@gmail.com>
2019-04-29 13:47:25 -0300

    Revert "Insere resultados finais"
    This reverts commit bc07a03274944569cbe112a6b62ca682ecf35bb5.

--- pack/main.py
+++ pack/main.py
@@ -1,38 +1,31 @@
 import numpy as np
 import random
 from pack.funcaoAptidao import apt_func
-
 # matriz da populacao com 20 membros
 populacao = np.zeros((20, 20), dtype=np.float)
 
 # variaveis
-n_cidades = 20
-n_geracoes = 10000
+nCidades = 20
+nGeracoes = 10000
 
 # define populacao aleatoria
-for i in range(n_cidades):
-    populacao[i] = random.sample(range(n_cidades), n_cidades)
+for i in range(nCidades):
+    populacao[i] = random.sample(range(nCidades), nCidades)
 
-x = np.random.rand(1, n_cidades)
-y = np.random.rand(1, n_cidades)
+# for i in range(nCidades):
+#    print populacao[i]
 
-# chama a funcao apt_funcao, responsavel por calcular a
-lista_aptidao = apt_func(populacao[:, :], x, y, n_cidades)
+x = np.random.rand(1, nCidades)
+y = np.random.rand(1, nCidades)
 
-# print(lista_aptidao)
+lista_aptidao = apt_func(populacao[:, :], x, y, nCidades)
+print lista_aptidao
 
 populacao_ordenada = np.zeros((20, 20), dtype=np.float)
 
-for i in range(n_cidades):
+for i in range(nCidades):
     populacao_ordenada[i, :] = populacao[int(lista_aptidao[i,0]), :]
 
-# print(populacao_ordenada)
+print populacao_ordenada
 
-taxa_mutacao = 0.05
-
-# imprime resultados finais na janela de comando
-print("Tamanho da População: " + str(np.size(populacao_ordenada, 0)))
-print("Taxa de Mutacao: " + str(taxa_mutacao))
-print("Número de Cidades: " + str(n_cidades))
-print("Melhor Custo: " + str(lista_aptidao[0, 1]))
-print("Melhor Solução: " + str(populacao_ordenada[0, :]))
+# funcao de apitadao
