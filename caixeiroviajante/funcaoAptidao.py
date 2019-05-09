6d72d92f010871194aa083c13b86f096fb364693
Alan Boaventura <alanboaventurab@gmail.com>
2019-05-08 21:03:48 -0300

    Ajustes finais na documentação

--- caixeiroviajante/funcaoAptidao.py
+++ caixeiroviajante/funcaoAptidao.py
@@ -5,28 +5,28 @@ import numpy
 
 
 def apt_func(populacao, v_aptidaoancia_cidade, n_rotas):
-    # Gera a matriz 20x21 da população onde a última coluna é a cópia da primeira coluna
-    # A última coluna é igual a primeira pois o caixeiro viajante precisa retornar a cidade original
+    # Gera a matriz 20x21 da população onde a última coluna é a cópia da primeira coluna.
+    # A última coluna é igual a primeira pois o caixeiro viajante precisa retornar a cidade original.
     tour = numpy.c_[populacao, populacao[:, 0]]
 
-    # Cria uma matrix 20x2 para receber a aptidão de cada cromossomo
-    # A primeira coluna corresponde ao índice do cromossomo na matriz de população
+    # Cria uma matrix 20x2 para receber a aptidão de cada cromossomo.
+    # A primeira coluna corresponde ao índice do cromossomo na matriz de população.
     # A segunda coluna representa o valor de aptidão
     v_aptidao = numpy.zeros((20, 2), dtype=numpy.float)
 
-    # Percorre o total de rotas existentes no algoritmo. Neste exemplo, cada rota representa um membro da população
+    # Percorre o total de rotas existentes no algoritmo. Neste exemplo, cada rota representa um membro da população.
     for i in range(0, n_rotas):
         v_aptidao[i, 0] = i
         v_aptidao[i, 1] = 0
-        # Para cada cidade calcula a v_aptidaoância entre ela e a próxima cidade
+        # Para cada cidade calcula a v_aptidaoância entre ela e a próxima cidade.
         for j in range(0, n_rotas):
             v_aptidao[i, 1] += v_aptidaoancia_cidade[int(tour[i, j]), int(tour[i, j + 1])]
 
-    # Loop para converter os índices do vetor de aptidão em inteiros
+    # Loop para converter os índices do vetor de aptidão em inteiros.
     for i in range(0, n_rotas):
         v_aptidao[i, 0] = int(v_aptidao[i, 0])
 
-    # Utiliza a função sorted para ordenar a matriz pela segunda coluna, correspondente ao valor de aptidão
+    # Utiliza a função sorted para ordenar a matriz pela segunda coluna, correspondente ao valor de aptidão.
     v_aptidao = sorted(v_aptidao, key=lambda x: x[1])
     v_aptidao = numpy.array(v_aptidao)
 
--- caixeiroviajante/main.py
+++ caixeiroviajante/main.py
@@ -9,33 +9,33 @@ import matplotlib.pyplot
 import sys
 import time
 import math
-# from caixeiroviajante.funcaoAptidao import apt_func
 from funcaoAptidao import apt_func
 
-# Esse algoritmo tem como objetivo realizar a implementação de um algoritmo genético baseado no caixeiro viajante
+# Esse algoritmo tem como objetivo realizar a implementação de um algoritmo genético baseado no caixeiro viajante.
+# Autores: Alan Boaventura e Lucas Carvalho.
 
-# Salva o instante de início da execução do algoritmo
+# Salva o instante de início da execução do algoritmo.
 start = time.time()
 
-# Cria a matriz da populacção com 20 membros
+# Cria a matriz da populacção com 20 membros.
 populacao = numpy.zeros((20, 20), dtype=numpy.float)
 
-# Número total de indivíduos na população
+# Número total de indivíduos na população.
 n_individuos = 20
-# Total de gerações que o algoritmo irá executar
+# Total de gerações que o algoritmo irá executar.
 n_geracoes = 10000
-# Essa variação está aqui para representar a possibilidade de cada membro ser escolhido para mutação
+# Essa variação está aqui para representar a possibilidade de cada membro ser escolhido para mutação.
 taxa_mutacao = 0.05
 
-# Habita a população com membros aleatórios
+# Habita a população com membros aleatórios.
 for i in range(n_individuos):
     populacao[i] = random.sample(range(n_individuos), n_individuos)
 
-# As variáveis x e y representam os vetores com a posição de cada cidade
+# As variáveis x e y representam os vetores com a posição de cada cidade.
 x = numpy.random.rand(1, n_individuos)
 y = numpy.random.rand(1, n_individuos)
 
-# Cria matriz que irá representar a distância entre as cidades
+# Cria matriz que irá representar a distância entre as cidades.
 distancia_cidade = numpy.zeros((20, 20), dtype=numpy.float)
 
 # Calcula a distância entre cada cidade
@@ -43,13 +43,13 @@ for i in range(0, n_individuos):
     for j in range(0, n_individuos):
         distancia_cidade[i, j] = math.sqrt(math.pow(x[0, i] - x[0, j], 2) + math.pow(y[0, i] - y[0, j], 2))
 
-# Utiliza a função apt_funcao, responsável por calcular a aptidão de cada indivíduo
+# Utiliza a função apt_funcao, responsável por calcular a aptidão de cada indivíduo.
 lista_aptidao = apt_func(populacao[:, :], distancia_cidade[:, :], n_individuos)
 
-# Cria o array que irá armazenar a população ordenada por resultado da função de aptidão
+# Cria o array que irá armazenar a população ordenada por resultado da função de aptidão.
 populacao_ordenada = numpy.zeros((20, 20), dtype=numpy.float)
 
-# Ordena a população de acordo com o resultado da execução da função de aptidão
+# Ordena a população de acordo com o resultado da execução da função de aptidão.
 for i in range(n_individuos):
     populacao_ordenada[i, :] = populacao[int(lista_aptidao[i, 0]), :]
 
@@ -66,52 +66,52 @@ roleta = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
           8, 8,
           9]
 
-# Array que será utilizado como referência para montagem do gráfico no final da execução do algoritmo
+# Array que será utilizado como referência para montagem do gráfico no final da execução do algoritmo.
 array_plot = []
 
-# Índice da geração para controle do laço de repetição
+# Índice da geração para controle do laço de repetição.
 indice_geracao = 0
 
 while indice_geracao < n_geracoes:
-    # Sorteia os números que serão utilizados na roleta
-    # São escolhidos dois grupos de pais com 5 indivíduos em cada
+    # Sorteia os números que serão utilizados na roleta.
+    # São escolhidos dois grupos de pais com 5 indivíduos em cada.
     grupo_pais1 = random.sample(range(0, 55), 5)
     grupo_pais2 = random.sample(range(0, 55), 5)
 
-    # Essas variáveis serão utilizadas para indicar a posição dos filhos na matriz da população
+    # Essas variáveis serão utilizadas para indicar a posição dos filhos na matriz da população.
     posicao_filho1 = 10
     posicao_filho2 = 11
     
-    # Variável utilizada no controle de crossover entre os pais
+    # Variável utilizada no controle de crossover entre os pais.
     i_pais = 0
     while i_pais < 5:
-        # Realiza uma cópia do filho com base no índice retirado da roleta
+        # Realiza uma cópia do filho com base no índice retirado da roleta.
         filho1 = copy.deepcopy(populacao_ordenada[roleta[grupo_pais1[i_pais]], :])
         filho2 = copy.deepcopy(populacao_ordenada[roleta[grupo_pais2[i_pais]], :])
 
-        # Define uma posição aleatória de corte para ser utilizado na técnica de cycle
+        # Define uma posição aleatória de corte para ser utilizado na técnica de cycle.
         posicao_corte = random.randint(0, 19)
 
-        # Busca o gene (cidade) que será trocado em cada filho
+        # Busca o gene (cidade) que será trocado em cada filho.
         corte1 = filho1[posicao_corte]
         corte2 = filho2[posicao_corte]
 
-        # Realiza a troca das cidades selecionadas
+        # Realiza a troca das cidades selecionadas.
         filho1[posicao_corte] = corte2
         filho2[posicao_corte] = corte1
 
-        # Índice criado para evitar que op rogram
+        # Índice criado para evitar que op rogram.
         indice_repetido_ant = copy.deepcopy(posicao_corte)
 
         # Após feita a primeira troca com o método cycle existe a possibilidade de um cidade (gene) estar repetida em um
         # indivíduo (cromossomo). Essa repetição serve para continuar trocando as cidades repetidas do primeiro filho,
-        # até que não existe mais nenhum repetido
+        # até que não exista mais nenhum repetido.
         while True:
             # Identifica se existe cidade repetida no primeiro filho
             cidade_repetida = [item for item, count in collections.Counter(filho1).items() if count > 1]
             if cidade_repetida:
                 for indice_repetido, e in list(enumerate(filho1)):
-                    # Encontra o índice da cidade repetida do filho1 e troca com o filho2
+                    # Encontra o índice da cidade repetida do filho1 e troca com o filho2.
                     if e == cidade_repetida and indice_repetido != indice_repetido_ant:
                         corte_repetido1 = filho1[indice_repetido]
                         corte_repetido2 = filho2[indice_repetido]
@@ -119,38 +119,39 @@ while indice_geracao < n_geracoes:
                         filho1[indice_repetido] = corte_repetido2
                         filho2[indice_repetido] = corte_repetido1
 
-                        # Copia o índice em que foi realizada a troca para evitar loop, trocando sempre a mesma posição
+                        # Copia o índice em que foi realizada a troca para evitar loop, trocando sempre a mesma posição.
                         indice_repetido_ant = copy.deepcopy(indice_repetido)
                         break
             else:
                 break
 
-        # Insere os filhos na população
+        # Insere os filhos na população.
         populacao_ordenada[posicao_filho1, :] = copy.deepcopy(filho1)
         populacao_ordenada[posicao_filho2, :] = copy.deepcopy(filho2)
         posicao_filho1 += 2
         posicao_filho2 += 2
         i_pais += 1
 
-    # Mutação da população
-    # Seleciona aleatoriamente um indivíduo para sofrer mutação
+    # Mutação da população.
+
+    # Seleciona aleatoriamente um indivíduo para sofrer mutação.
     i_mutacao = random.randint(0, 19)
-    # Seleciona as duas cidades que terão sua posição trocada
+    # Seleciona as duas cidades que terão sua posição trocada.
     pos_mutacao = random.sample(range(0, 19), 2)
 
-    # Realiza a mutação
+    # Realiza a mutação.
     mut_a = copy.deepcopy(populacao_ordenada[i_mutacao, pos_mutacao[0]])
     mut_b = copy.deepcopy(populacao_ordenada[i_mutacao, pos_mutacao[1]])
 
     populacao_ordenada[i_mutacao, pos_mutacao[0]] = mut_b
     populacao_ordenada[i_mutacao, pos_mutacao[1]] = mut_a
 
-    # Chama novamente a função parar calcular a aptidão da população, agora com os filhos
+    # Chama novamente a função parar calcular a aptidão da população, agora com os filhos.
     lista_aptidao = apt_func(populacao_ordenada[:, :], distancia_cidade[:, :], n_individuos)
     array_plot.append(lista_aptidao[0, 1])
 
     populacao_ordenada_aux = copy.deepcopy(populacao_ordenada)
-    # Ordena a população pelo vetor de aptidão
+    # Ordena a população pelo vetor de aptidão.
     for i in range(n_individuos):
         populacao_ordenada[i, :] = populacao_ordenada_aux[int(lista_aptidao[i, 0]), :]
 
@@ -159,10 +160,10 @@ while indice_geracao < n_geracoes:
 # Salva o instante de "término" da execução do algoritmo.
 end = time.time()
 
-# "Calcula" o tempo (em segundos") de execução do algoritmo.
+# "Calcula" o tempo (em segundos) de execução do algoritmo.
 executionTime = str("%.1f" % (end - start))
 
-# Imprime resultados finais na janela de comando e exibe o gráfico.
+# Imprime resultados finais na janela de comando.
 print("")
 print("O algoritmo levou " + executionTime + " segundos para executar")
 print("")
@@ -177,10 +178,10 @@ print("Melhor Custo: " + str(lista_aptidao[0, 1]))
 print("Melhor Solucao: " + str(populacao_ordenada[0, :]))
 print("")
 
-# Exibe gráfico com as melhores aptidões
+# Exibe gráfico com as melhores aptidões.
 matplotlib.pyplot.ylim(3, 10)
 matplotlib.pyplot.plot(array_plot)
 matplotlib.pyplot.show()
 
-# Finaliza a execução do algoritmo
+# Finaliza a execução do algoritmo.
 sys.exit()
